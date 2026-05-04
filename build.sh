f3c7ebf6c90409e3ceec65ec845d6432ddffb07f
SPARS2310 <spars0105@gmail.com>
2026-05-03 18:15:15 -0700

    Ajuste de rutas de base de datos y permisos

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Crear la carpeta de la base de datos si no existe y darle permisos
+mkdir -p /var/data
+chmod 777 /var/data
+
 pip install --upgrade pip
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
 python manage.py migrate
