69ad04f1d327f5591435643a454f746bc42b4a14
SPARS2310 <spars0105@gmail.com>
2026-05-03 18:20:50 -0700

    Forzando creación de carpeta de base de datos

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
+# Salir si hay un error
 set -o errexit
 
-# Crear la carpeta de la base de datos si no existe y darle permisos
+# ESTA ES LA CLAVE: Crear la carpeta y darle permisos totales de escritura
 mkdir -p /var/data
 chmod 777 /var/data
 
-pip install --upgrade pip
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python manage.py collectstatic --noinput
 python manage.py migrate
